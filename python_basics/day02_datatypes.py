ff13832fab38752db6988f17b57e8f7317a7cf45
jaisriwarad-hub <jaisriwarad@gmail.com>
2026-01-17 15:32:14 +0530

    Create day02_datatypes.py

---
+++ python_basics/day02_datatypes.py
@@ -0,0 +1,32 @@
+# Day 02 - Data Types in Python
+
+# Integer
+age = 20
+print(type(age))
+
+# Float
+height = 5.4
+print(type(height))
+
+# String
+name = "Simba"
+print(type(name))
+
+# Boolean
+is_learning = True
+print(type(is_learning))
+
+# List
+marks = [85, 90, 88]
+print(type(marks))
+
+# Tuple
+coordinates = (10, 20)
+print(type(coordinates))
+
+# Dictionary
+student = {
+    "name": "Jaisri",
+    "age": 20
+}
+print(type(student))
